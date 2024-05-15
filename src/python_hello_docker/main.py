1331a4a6edbf047fa771126b70ca24c622e40c79
Stefan Boos <kontakt@boos.systems>
2024-05-15 05:37:59 +0200

    feat: main.py is compatible with python 2.7

--- src/python_hello_docker/main.py
+++ src/python_hello_docker/main.py
@@ -3,8 +3,8 @@ import sys
 
 def python_version_message():
     major, minor, _, _, _ = sys.version_info
-    python_version = f"{major}.{minor}"
-    return f"You are running python version {python_version}"
+    python_version = "{}.{}".format(major, minor)
+    return "You are running python version {}".format(python_version)
 
 
 if __name__ == "__main__":
